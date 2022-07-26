33ebc2a1e1cda87581eb42bf93b4294be2766607
synsi23b <mail@himitsu.dev>
2022-07-26 11:06:18 +0900

    insert jpy rate to mongodb

---
+++ import.py
@@ -0,0 +1,39 @@
+import xmltodict
+from datetime import datetime
+import requests
+from mongodb import insert_many_jpy_rate, get_last_jpy_rate
+
+# first import japan yen exchange rate from european cental bank
+# download https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/jpy.xml
+# https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/eurofxref-graph-jpy.en.html
+
+#jpyecb = requests.get("https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/jpy.xml")
+#open("jpy_exchange_rate_historical.xml", "wb").write(jpyecb.content)
+
+jpy = None
+with open("jpy_exchange_rate_historical.xml") as jpyxml:
+    jpy = xmltodict.parse(jpyxml.read())
+
+
+MINYEAR = datetime(year=2019, month=12, day=31)
+try:
+    mininsert = get_last_jpy_rate()["date"]
+    print(f"Last known jpy rate value {mininsert}")
+except:
+    print("no mininsert value! Using default")
+    mininsert = MINYEAR
+
+dateratelist = []
+for dailyrate in jpy["CompactData"]["DataSet"]["Series"]["Obs"]:
+    #print(dailyrate)
+    date = datetime.strptime(dailyrate["@TIME_PERIOD"], "%Y-%m-%d")
+    # ignore very old or existing data
+    if date <= mininsert:
+        continue
+    # build ratelist objects
+    dateratelist.append({"date": date, "rate": float(dailyrate["@OBS_VALUE"])})
+if dateratelist:
+    insert_many_jpy_rate(dateratelist)
+else:
+    print("no update to euro jpy rate")
+    
--- mongodb.py
+++ mongodb.py
@@ -1,5 +1,38 @@
 from dotenv import load_dotenv
+import pymongo
+import os
 
 
-load_dotenv()
+_db = None
 
+
+def get_db():
+    global _db
+    if _db is None:
+        load_dotenv()
+        client = pymongo.MongoClient(os.getenv('MONGOCON'))
+        _db = client.get_database("stocksdb")
+    return _db
+
+
+def check_res(res):
+    print(res, f"was acknowledged: {res.acknowledged}")
+
+
+def get_last_jpy_rate():
+    db = get_db()
+    return db["eur2jpy"].find_one(sort=[("date", -1)])
+
+
+def insert_many_jpy_rate(dateratelist):
+    db = get_db()
+    db.create_collection("eur2jpy", timeseries={
+                "timeField": "date",
+                "granularity": "hours"
+            })
+    check_res(db["eur2jpy"].insert_many(dateratelist))
+
+
+if __name__ == "__main__":
+    date = get_last_jpy_rate()["date"]
+    print(date)
